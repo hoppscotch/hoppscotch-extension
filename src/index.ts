9404f4ca8f5fb31613bcbc71f3cfc28c1c248925
Andrew Bastin <andrewbastin.k@gmail.com>
2020-01-14 12:05:24 -0500

    Added version querying

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import axios, { AxiosRequestConfig } from "axios";
 
+const VERSION = { major: 0, minor: 1 };
+
+
 interface PWChromeMessage<T> {
-  messageType: "send-req" | "recv-req"; 
+  messageType: "send-req" | "recv-req" | "send-version" | "recv-version"; 
   data: T;
 }
 
 interface SendRequestMessageData {
   config: AxiosRequestConfig;
+}
+
+interface VersionRequestMessageData {
+  version: { major: number, minor: number };
 }
 
 
@@ -28,9 +35,15 @@ chrome.runtime.onMessageExternal.addListener((message: PWChromeMessage<any>, _se
   console.log(message);
   if (message.messageType === "send-req") {
     handleSendRequestMessage(message).then(sendResponse);
+  } else if (message.messageType === "send-version") {
+    return <PWChromeMessage<VersionRequestMessageData>>{
+      messageType: "recv-version",
+      data: {
+        version: VERSION
+      }
+    }
   }
+
 
   return true;
 });
-
-console.log("Whaddup my glibbglobbss!!!!");

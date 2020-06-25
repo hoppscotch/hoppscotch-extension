0d635b48982c3deebe7a7d6d5fbfcb6cc56868df
Andrew Bastin <andrewbastin.k@gmail.com>
2020-06-25 14:48:18 -0400

    Bump version to 0.13

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -28,7 +28,7 @@ window.addEventListener('message', ev => {
   }
 });
 
-const VERSION = { major: 0, minor: 12 };
+const VERSION = { major: 0, minor: 13 };
 
 const script = document.createElement('script');
 script.textContent = `

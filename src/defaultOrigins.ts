7694adbdb2e327fcb70162c9101fd129c1810ffa
Allen Francis <lallenfrancisl@gmail.com>
2024-01-17 15:18:48 +0530

    feat: add localhost origins to default origins

--- src/defaultOrigins.ts
+++ src/defaultOrigins.ts
@@ -1,1 +1,7 @@
-export const DEFAULT_ORIGIN_LIST = ["https://hoppscotch.io"]
+export const DEFAULT_ORIGIN_LIST = [
+  "https://hoppscotch.io",
+  "http://localhost",
+  "https://localhost",
+  "http://127.0.0.1",
+  "https://127.0.0.1",
+]
